5aa4a2d20f6d43fa9beeecbbff95ef2861ba482c
John M <github.fox-io@foxdata.io>
2023-03-28 16:53:45 -0500

    Redo sprites
    Make sprites better.
    
    Added variables for oft used values.

--- main.py
+++ main.py
@@ -6,27 +6,32 @@ pygame.init()
 
 class Catch:
     def __init__(self):
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen_width = 400
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("Catch")
         self.screen.fill((0, 0, 0))
 
+        self.player_sprite_size = 32
         self.player_surface = pygame.image.load("fox.png")
         self.player_sprite = pygame.sprite.Sprite()
         self.player_sprite.image = self.player_surface
         self.player_sprite.rect = self.player_surface.get_rect()
-        self.player_sprite.rect.x = 400-(32/2)
-        self.player_sprite.rect.y = 600-64
+        self.player_sprite.rect.x = self.screen_width / 2 - (self.player_sprite_size / 2)
+        self.player_sprite.rect.y = self.screen_height - (self.player_sprite_size * 2)
 
-        self.target_surface = pygame.image.load("rocket.png")
+        self.target_sprite_size = 32
+        self.target_surface = pygame.image.load("target.png")
 
         self.font = pygame.font.SysFont("Arial", 32)
         self.score = 0
 
-        self.move_speed = 5
+        self.player_move_speed = 6
         self.moving_left = False
         self.moving_right = False
         self.clock = pygame.time.Clock()
 
+        self.target_move_speed = 3
         self.target_sprite_group = pygame.sprite.Group()
 
         self.sprite_group = pygame.sprite.Group()
@@ -38,7 +43,7 @@ class Catch:
         target_sprite = pygame.sprite.Sprite()
         target_sprite.image = self.target_surface
         target_sprite.rect = self.target_surface.get_rect()
-        target_sprite.rect.x = random.randint(0 + 32, self.screen.get_width() - 32)
+        target_sprite.rect.x = random.randint(self.target_sprite_size, self.screen.get_width() - self.target_sprite_size)
         target_sprite.rect.y = 0
         return target_sprite
 
@@ -62,16 +67,16 @@ while running:
                 app.moving_right = False
 
     if app.moving_left:
-        app.player_sprite.rect.x -= app.move_speed
+        app.player_sprite.rect.x -= app.player_move_speed
     elif app.moving_right:
-        app.player_sprite.rect.x += app.move_speed
+        app.player_sprite.rect.x += app.player_move_speed
 
     if pygame.time.get_ticks() % 60 == 0:
         target_object = app.create_target()
         app.target_sprite_group.add(target_object)
 
     for target in app.target_sprite_group.sprites():
-        target.rect.y += 5
+        target.rect.y += app.target_move_speed
 
         if target.rect.y > app.screen.get_height():
             app.target_sprite_group.remove(target)
